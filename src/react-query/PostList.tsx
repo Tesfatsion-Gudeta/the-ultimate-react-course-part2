fca068818b29afd009666dba5d99648af509e4ea
Tesfatsion-Gudeta <tesfatsiongudeta7@gmail.com>
2024-11-28 18:59:18 +0300

    paginated queries

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
-  const [userId, setUserId] = useState<number>();
-  const { data: posts, error, isLoading } = usePosts(userId);
+  // const [userId, setUserId] = useState<number>();
+  const pageSize = 10;
+  const [page, setPage] = useState(1);
+  const { data: posts, error, isLoading } = usePosts({ page, pageSize });
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
 
   return (
     <>
+      {/* filtering posts by user
       <select
         onChange={(event) => setUserId(parseInt(event.target.value))}
         value={userId}
@@ -19,7 +22,7 @@ const PostList = () => {
         <option value="1">user 1</option>
         <option value="2">user 2</option>
         <option value="3">user 3</option>
-      </select>
+      </select> */}
 
       <ul className="list-group">
         {posts?.map((post) => (
@@ -28,6 +31,19 @@ const PostList = () => {
           </li>
         ))}
       </ul>
+      <button
+        disabled={page === 1}
+        onClick={() => setPage(page - 1)}
+        className="btn btn-primary my-3"
+      >
+        Previous
+      </button>
+      <button
+        onClick={() => setPage(page + 1)}
+        className="btn btn-primary my-3 ms-1"
+      >
+        Next
+      </button>
     </>
   );
 };

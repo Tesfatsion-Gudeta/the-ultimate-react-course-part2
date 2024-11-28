2b0d3ef34191413d3637abc2be581a6f32b2cf1c
Tesfatsion-Gudeta <tesfatsiongudeta7@gmail.com>
2024-11-28 19:53:00 +0300

    infinite queries with button

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import usePosts from "./hooks/usePosts";
+import React from "react";
 
 const PostList = () => {
   // const [userId, setUserId] = useState<number>();
   const pageSize = 10;
-  const [page, setPage] = useState(1);
-  const { data: posts, error, isLoading } = usePosts({ page, pageSize });
+  // const [page, setPage] = useState(1);
+  const {
+    data: posts,
+    error,
+    isLoading,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = usePosts({ pageSize });
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
@@ -25,24 +32,29 @@ const PostList = () => {
       </select> */}
 
       <ul className="list-group">
-        {posts?.map((post) => (
-          <li key={post.id} className="list-group-item">
-            {post.title}
-          </li>
+        {posts.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </React.Fragment>
         ))}
       </ul>
-      <button
+      {/* <button
         disabled={page === 1}
         onClick={() => setPage(page - 1)}
         className="btn btn-primary my-3"
       >
         Previous
-      </button>
+      </button> */}
       <button
-        onClick={() => setPage(page + 1)}
+        onClick={() => fetchNextPage()}
         className="btn btn-primary my-3 ms-1"
+        disabled={isFetchingNextPage}
       >
-        Next
+        {isFetchingNextPage ? "Loading..." : "Load More"}
       </button>
     </>
   );

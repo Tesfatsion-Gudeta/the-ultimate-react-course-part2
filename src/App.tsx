ccb3feab9b01b641db12616a6ae739747ad86b49
Tesfatsion-Gudeta <tesfatsiongudeta7@gmail.com>
2024-12-01 17:13:21 +0300

    organizing code related to tasks-code organization for scalabilty and maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import "./App.css";
 import AuthProvider from "./state-management/AuthProvider";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
-import TasksProvider from "./state-management/TasksProvider";
-
+import { TasksProvider } from "./state-management/tasks";
 function App() {
   // const [tasks, tasksDispatch] = useReducer(taskReducer, []);
   // const [user, authDispatch] = useReducer(authReducer, "");

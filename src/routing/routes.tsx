9e9ade4ba924aee910e219b6544fa081030dabd3
Tesfatsion-Gudeta <tesfatsiongudeta7@gmail.com>
2024-12-02 17:08:08 +0300

    handling error

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,15 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
-import UserList from "./UserList";
-import ContactPage from "./ContactPage";
 import UserDetail from "./UserDetail";
 import Layout from "./Layout";
 import Userspage from "./Userspage";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {

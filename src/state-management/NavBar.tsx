51afa621d38c434ceb57914fd5cbc4633843d269
Tesfatsion-Gudeta <tesfatsiongudeta7@gmail.com>
2024-12-02 07:17:45 +0300

    replacing the counter state implentation with zustand store

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,12 +1,15 @@
 import { useContext } from "react";
 import LoginStatus from "./auth/LoginStatus";
 import TaskContext from "./tasks/tasksContext";
+import useCounterStore from "./counter/store";
 
 const NavBar = () => {
   const { tasks } = useContext(TaskContext);
+  const { counter } = useCounterStore();
   return (
     <nav className="navbar d-flex justify-content-between">
       <span className="badge text-bg-secondary">{tasks.length}</span>
+      <span className="badge text-bg-primary">{counter}</span>
       <LoginStatus />
     </nav>
   );
--- src/state-management/counter/Counter.tsx
+++ src/state-management/counter/Counter.tsx
@@ -1,20 +1,25 @@
 import { useReducer } from "react";
-import counterReducer from "./counterReducer";
+import useCounterStore from "./store";
 
 const Counter = () => {
   // const [value, setValue] = useState(0);
-  const [value, dispatch] = useReducer(counterReducer, 0);
+  // const [value, dispatch] = useReducer(counterReducer, 0);   //replaced this implementation using zustand store
+
+  const { counter, increment, reset } = useCounterStore();
+
   return (
     <div>
-      Counter ({value})
+      Counter ({counter})
       <button
-        onClick={() => dispatch({ type: "INCREMENT" })}
+        // onClick={() => dispatch({ type: "INCREMENT" })}
+        onClick={() => increment()}
         className="btn btn-primary mx-1"
       >
         Increment
       </button>
       <button
-        onClick={() => dispatch({ type: "RESET" })}
+        // onClick={() => dispatch({ type: "RESET" })}
+        onClick={() => reset()}
         className="btn btn-primary mx-1"
       >
         Reset
